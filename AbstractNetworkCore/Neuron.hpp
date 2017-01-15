f2f79ff809c1d37802633d3ebd6ba035b4d2e5ec
Vania3690 <Vanya.Krisak@gmail.com>
2017-01-15 16:47:12 +0200

    Core Neuron implementation.

--- AbstractNetworkCore/Neuron.hpp
+++ AbstractNetworkCore/Neuron.hpp
@@ -1,1 +1,52 @@
 #pragma once
+
+#include <initializer_list>
+#include <vector>
+	namespace std {
+		template<class _Elem> class initializer_list;
+	}
+#define Container std::vector
+
+namespace MNN {
+	template <typename T>
+	class AbstractNeuron {
+	private:
+		T m_value;
+		bool m_isValuated;
+	protected:
+		Container<T> m_inputs;
+	protected:
+		virtual T& calculate() abstract;
+		static T& normalize(T& input) {
+			return input;
+		}
+	public:
+		AbstractNeuron(const T& value) : m_isValuated(true), m_value(value) {}
+		AbstractNeuron() : m_isValuated(false) {}
+
+		void setInputs(const std::initializer_list<T>& l) {
+			m_inputs.clear();
+			m_inputs.reserve(l.size());
+			for (T t : l) m_inputs.push_back(t);
+		}
+
+		T value() {
+			if (m_isValuated)
+				return m_value;
+			else {
+				m_isValuated = true;
+				return m_value = normalize(calculate());
+			}
+		}
+	};
+
+	class Neuron : public AbstractNeuron<float> {
+	protected:
+		virtual float& calculate() override {
+			float result = 0;
+			for (float t : m_inputs)
+				result += t;
+			return result;
+		}
+	};
+}
--- GuiTestingImplementation/main.cpp
+++ GuiTestingImplementation/main.cpp
@@ -1,9 +1,16 @@
 #include "GuiTestingImplementation.h"
 #include <QtWidgets/QApplication>
 
+#include "Neuron.hpp"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+	MNN::Neuron n;
+	n.setInputs({1,2,3,4});
+	float p = n.value();
+
     GuiTestingImplementation w;
     w.show();
     return a.exec();
