2dad7d7781c00648295cce920f47ff7513f8b058
Cvelth <cvelth.mail@gmail.com>
2017-11-04 00:21:11 +0200

    Exceptions + Constants -> Shared

--- AbstractNetworkCore/Constants.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define Type float
-
-#include <vector>
-#define NeuronContainer std::vector
-#define WeightContainer std::vector
--- AbstractNetworkCore/Shared.hpp
+++ AbstractNetworkCore/Shared.hpp
@@ -1,4 +1,10 @@
 #pragma once
+#define Type float
+
+#include <vector>
+#define NeuronContainer std::vector
+#define WeightContainer std::vector
+
 #include <exception>
 namespace mnn {
 	namespace Exceptions {
