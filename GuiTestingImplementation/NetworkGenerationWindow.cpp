25732ec3aa7ab957f2c50a03c8367600fec485da
Cvelth <cvelth.mail@gmail.com>
2017-11-04 21:44:15 +0200

    NetworkGenerationWindow style was improved. It now uses mnn::AbstractNetwork instead of mnn::AbstractLayerNetwork.

--- GuiTestingImplementation/NetworkGenerationWindow.cpp
+++ GuiTestingImplementation/NetworkGenerationWindow.cpp
@@ -1,13 +1,10 @@
-#include "NetworkGenerationWindow.h"
-
+#include "NetworkGenerationWindow.hpp"
 bool NetworkGenerationWindow::isGeneratorInitialized = false;
 mnnt::RealRandomEngine* NetworkGenerationWindow::m_random_generator = nullptr;
-
 NetworkGenerationWindow::~NetworkGenerationWindow() {
 	if (isGeneratorInitialized) delete m_random_generator;
 	isGeneratorInitialized = false;
 }
-
 void NetworkGenerationWindow::hideAdditionalFields() {
 	ui.eta_label->hide();
 	ui.eta->hide();
@@ -18,7 +15,6 @@ void NetworkGenerationWindow::hideAdditionalFields() {
 	ui.moreButton->setText("More");
 	areAdditionalFieldsShown = false;
 }
-
 void NetworkGenerationWindow::showAdditionalFields() {
 	ui.eta_label->show();
 	ui.eta->show();
@@ -29,49 +25,23 @@ void NetworkGenerationWindow::showAdditionalFields() {
 	ui.moreButton->setText("Less");
 	areAdditionalFieldsShown = true;
 }
-
 void NetworkGenerationWindow::toggleAdditionalFields() {
 	if (areAdditionalFieldsShown)
 		hideAdditionalFields();
 	else
 		showAdditionalFields();
 }
-
-#include "Automatization.hpp"
-void NetworkGenerationWindow::startGeneration() {
-	auto network = mnn::generateTypicalLayerNeuralNetwork(ui.inputs->value(), ui.outputs->value(), ui.hidden->value(), ui.per_hidden->value(),
-														  chooseConnection(ui.connection->currentIndex()), chooseDefaultWeights(ui.default_weight->currentIndex()),
-														  ui.eta->value(), ui.alpha->value());
-	emit returnNetwork(network);
-	delete this;
-}
-
-mnn::ConnectionPattern NetworkGenerationWindow::chooseConnection(size_t index) {
-	switch (index) {
-		default:
-		case 0:
-			return mnn::ConnectionPattern::EachFromPreviousLayerWithBias;
-		case 1:
-			return mnn::ConnectionPattern::EachFromPreviousLayerWithoutBias;
-		case 2:
-			return mnn::ConnectionPattern::NoDefaultConnection;
-	}
-}
-
-#include <AbstractLayerNetwork.hpp>
-NetworkGenerationWindow::NetworkGenerationWindow(QObject* receiver, std::function<void(mnn::AbstractLayerNetwork*)> slot, QWidget *parent)
+#include <AbstractNetwork.hpp>
+NetworkGenerationWindow::NetworkGenerationWindow(QObject* receiver, std::function<void(mnn::AbstractNetwork*)> slot, QWidget *parent)
 	: QWidget(parent) {
 	ui.setupUi(this);
 
 	hideAdditionalFields();
-
 	connect(ui.moreButton, &QPushButton::clicked, this, &NetworkGenerationWindow::toggleAdditionalFields);
 	connect(ui.generateButton, &QPushButton::clicked, this, &NetworkGenerationWindow::startGeneration);
 	connect(this, &NetworkGenerationWindow::returnNetwork, receiver, slot);
-
 	show();
 }
-
 #include "AbstractNeuron.hpp"
 #include "RandomEngine.hpp"
 std::function<float(mnn::AbstractNeuron const&, mnn::AbstractNeuron const&)> NetworkGenerationWindow::chooseDefaultWeights(size_t index) {
@@ -122,4 +92,24 @@ std::function<float(mnn::AbstractNeuron const&, mnn::AbstractNeuron const&)> Net
 			};
 	}
 	return std::function<float(mnn::AbstractNeuron const&, mnn::AbstractNeuron const&)>();
+}
+#include "Automatization.hpp"
+mnn::ConnectionPattern NetworkGenerationWindow::chooseConnection(size_t index) {
+	switch (index) {
+	default:
+	case 0:
+		return mnn::ConnectionPattern::EachFromPreviousLayerWithBias;
+	case 1:
+		return mnn::ConnectionPattern::EachFromPreviousLayerWithoutBias;
+	case 2:
+		return mnn::ConnectionPattern::NoDefaultConnection;
+	}
+}
+#include "AbstractLayerNetwork.hpp"
+void NetworkGenerationWindow::startGeneration() {
+	mnn::AbstractNetwork *network = mnn::generateTypicalLayerNeuralNetwork(ui.inputs->value(), ui.outputs->value(), ui.hidden->value(), ui.per_hidden->value(),
+		chooseConnection(ui.connection->currentIndex()), chooseDefaultWeights(ui.default_weight->currentIndex()),
+		ui.eta->value(), ui.alpha->value());
+	emit returnNetwork(network);
+	delete this;
 }
