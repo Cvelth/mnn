cd016194dace21a05c8c7e0ecc25fad248e1bbb7
Cvelth <cvelth.mail@gmail.com>
2017-11-04 21:01:39 +0200

    MultiField now correctly stores big(20+) amount of widgets. They are no longed resized and ScrollBars appear as it was intended.
    Inner exceptions now use the exception macros from "Share.hpp" to be compatible with all the other library macroses.
    
    Additional insignificant fixes and style improvement were added to MultiField, as well.

--- GuiTestingImplementation/MultiField.hpp
+++ GuiTestingImplementation/MultiField.hpp
@@ -1,26 +1,20 @@
 #pragma once
 #include <QtWidgets\QScrollArea>
 #include <QVector>
-
+#include "Shared.hpp"
 class QVBoxLayout;
 class QSpacerItem;
 class QDoubleSpinBox;
 using Field = QDoubleSpinBox;
-
-namespace Exceptions {
-	namespace MultiField {
-		class AbstractException {};
-		class BrokenSizeException : AbstractException {};
-		class NoMoreElementsException : BrokenSizeException {};
-	}
-}
+GenerateNewException(BrokenSizeException);
+GenerateNewException(NoMoreElementsException);
 
 class MultiField : public QScrollArea {
 public:
-	explicit MultiField(size_t number = 0, QWidget* parent = nullptr);
-	~MultiField();
+	explicit MultiField(size_t number = 0, QWidget *parent = nullptr);
+	inline ~MultiField() { destroy(); }
 
-	virtual void clear();
+	inline virtual void clear() { change(0); }
 	virtual void add();
 	virtual void add(size_t number);
 	virtual void remove();
@@ -37,8 +31,9 @@ protected:
 private:
 	size_t m_number;
 	QVector<Field*> m_fields;
-	QVBoxLayout* m_layout;
-	QSpacerItem* m_spacer;
+	QVBoxLayout *m_layout;
+	QSpacerItem *m_spacer;
+	QWidget *m_widget;
 
 	static const float minimum;
 	static const float maximum;
