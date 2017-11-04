75c65e5def91e4b06f8e9ca16d6822121efdb482
Cvelth <cvelth.mail@gmail.com>
2017-11-04 22:48:37 +0200

    Storage System was implemented. All what's left is to implement operator<<() and operaor>>() for network.

--- AbstractNetworkCore/Storage.cpp
+++ AbstractNetworkCore/Storage.cpp
@@ -1,7 +1,21 @@
 #include "Storage.hpp"
+#define StorageSystemVersionCode "MNN_v0.2.53_storage_v1.0"
+#include <fstream>
+#include "AbstractNetwork.hpp"
 void mnn::save_to_file(std::string &filename, AbstractNetwork *network) {
-
+	if (network == nullptr)
+		throw Exceptions::InvalidNetworkException();
+	std::ofstream f;
+	f.open(filename, std::ofstream::out);
+	f << StorageSystemVersionCode << ' ';
+	f << *network;
 }
-void mnn::load_from_file(std::string &filename, AbstractNetwork *network) {
-
+void mnn::load_from_file(std::string &filename, AbstractNetwork *network, bool ignoreUnsupportedMessage) {
+	std::ifstream f;
+	f.open(filename, std::ifstream::in);
+	std::string version;
+	f >> version;
+	if (!ignoreUnsupportedMessage && version != StorageSystemVersionCode)
+		throw Exceptions::UnsupportedMNNFile();
+	f >> *network;
 }
--- AbstractNetworkCore/Storage.hpp
+++ AbstractNetworkCore/Storage.hpp
@@ -1,7 +1,10 @@
 #pragma once
+#include "Shared.hpp"
 #include <string>
 namespace mnn {
+	GenerateNewException(InvalidNetworkException);
+	GenerateNewException(UnsupportedMNNFile);
 	class AbstractNetwork;
 	void save_to_file(std::string &filename, AbstractNetwork *network);
-	void load_from_file(std::string &filename, AbstractNetwork *network);
+	void load_from_file(std::string &filename, AbstractNetwork *network, bool ignoreUnsupportedMessage = false);
 }
