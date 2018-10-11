1ba81a23f9d2f39b792714f19cc70b090c7e58a0
cvelth <cvelth.mail@gmail.com>
2018-10-12 00:33:20 +0300

    ExplicitlyLinkedNeuron was implemented.

---
+++ mnn/ExplicitlyLinkedNetwork/Neuron.cpp
@@ -0,0 +1,40 @@
+#include "Neuron.hpp"
+void mnn::Neuron::calculate() {
+	Value temp = 0;
+	for (auto &t : m_links)
+		temp += t.unit->value() * t.weight;
+	value(temp);
+}
+
+void mnn::BackpropagationNeuron::calculate() {
+	Value temp = 0;
+	for (auto &t : m_links)
+		temp += t.unit->value() * t.weight;
+	value(temp);
+}
+
+#include <algorithm>
+mnn::Value mnn::BackpropagationNeuron::getWeightTo(BackpropagationNeuronInterface *neuron) {
+	if (auto it = std::find_if(m_links.cbegin(), m_links.cend(), [neuron](auto const& l) {
+		return l.unit == neuron;
+	}); it != m_links.cend())
+		return it->weight;
+	else
+		return 0.0;
+}
+void mnn::BackpropagationNeuron::recalculateWeights() {
+	for (auto &l : m_links) {
+		l.delta = m_eta * l.unit->value() * m_gradient
+			+ m_alpha * l.delta;
+		l();
+	}
+	changed();
+}
+void mnn::BackpropagationNeuron::calculateGradient(Value const& expectedValue) {
+	m_gradient = (expectedValue - value()) * normalization_derivative(value());
+}
+void mnn::BackpropagationNeuron::calculateGradient(std::function<Value(std::function<Value(BackpropagationNeuronInterface&)>)> gradient_sum) {
+	m_gradient = gradient_sum([this](auto &n) -> Value {
+		return n.getWeightTo(this) * n.gradient();
+	}) * normalization_derivative(value());
+}
---
+++ mnn/ExplicitlyLinkedNetwork/Neuron.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include "mnn/interfaces/ExplicitlyLinkedNeuronInterface.hpp"
+#include "mnn/interfaces/ExplicitLink.hpp"
+namespace mnn {
+	class Neuron : public NeuronInterface {
+	protected:
+		LinkContainer<ExplicitLink> m_links;
+		virtual void calculate() override;
+	public:
+		Neuron() : NeuronInterface() {}
+		Neuron(Value const& value) : NeuronInterface(value) {}
+		virtual ~Neuron() {}
+
+		virtual void link(NeuronInterface *i, Value const& weight = 1.f) override {
+			m_links.push_back(ExplicitLink(i, weight));
+			changed();
+		}
+		inline virtual void clear_links() override { m_links.clear(); changed(); }
+	};
+	class BackpropagationNeuron : public BackpropagationNeuronInterface {
+	protected:
+		LinkContainer<ExplicitBackpropagationLink> m_links;
+		virtual void calculate() override;
+	public:
+		BackpropagationNeuron() : BackpropagationNeuronInterface(0.15, 0.5) {}
+		BackpropagationNeuron(Value const& value)
+			: BackpropagationNeuronInterface(value, 0.15, 0.5) {}
+		BackpropagationNeuron(Value const& eta, Value const& alpha)
+			: BackpropagationNeuronInterface(eta, alpha) {}
+		BackpropagationNeuron(Value const& value, Value const& eta, Value const& alpha)
+			: BackpropagationNeuronInterface(value, eta, alpha) {}
+
+		virtual void calculateGradient(Value const& expectedValue) override;
+		virtual void calculateGradient(std::function<Value(std::function<Value(BackpropagationNeuronInterface&)>)> gradient_sum) override;
+		virtual Value getWeightTo(BackpropagationNeuronInterface* neuron) override;
+		virtual void recalculateWeights() override;
+
+		virtual void link(NeuronInterface *i, Value const& weight = 1.f) override {
+			m_links.push_back(ExplicitBackpropagationLink(i, weight));
+			changed();
+		}
+		inline virtual void clear_links() override { m_links.clear(); changed(); }
+	};
+}
