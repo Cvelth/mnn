66bfd123359b6a9bd055a7d04c8c9784952515ff
cvelth <cvelth.mail@gmail.com>
2018-10-11 22:49:28 +0300

    NeuralNetworkInterface was redesigned.

---
+++ Interfaces/NetworkInterface.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <vector>
+namespace mnn {
+	class NeuralNetworkInterface {
+	public:
+		using Value = double;
+		template <typename Type> using NeuronContainer = std::vector<Type>;
+
+		virtual ~NeuralNetworkInterface() {}
+
+		virtual NeuronContainer<Value>& inputs() = 0;
+		virtual NeuronContainer<Value> const& inputs() const = 0;
+		virtual NeuronContainer<Value>& outputs() = 0;
+		virtual NeuronContainer<Value> const& outputs() const = 0;
+
+		virtual void inputs(NeuronContainer<Value> const& _inputs, bool normalize = true) = 0;
+
+		virtual void process() = 0;
+		inline void process(NeuronContainer<Value> const& _inputs, bool normalize = true) {
+			inputs(_inputs, normalize);
+			process();
+		}
+
+		/* TO DO
+		friend std::ostream& operator<<(std::ostream &s, NetworkInterface const* n);
+		friend std::istream& operator>>(std::istream &s, NetworkInterface *&n);
+		*/
+	};
+
+	class BackpropagationNeuralNetworkInterface : public NeuralNetworkInterface {
+	public:
+		virtual void calculateGradients(NeuronContainer<Value> const& _outputs) = 0;
+		virtual void updateWeights() = 0;
+		inline void backpropagate(NeuronContainer<Value> const& _outputs) {
+			calculateGradients(_outputs);
+			updateWeights();
+		}
+	};
+}
