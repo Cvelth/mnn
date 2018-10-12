3703dfd34911ee97d1ff6bb01bb36563dc5e9b9d
cvelth <cvelth.mail@gmail.com>
2018-10-12 16:31:10 +0300

    ExplicitlyLinkedBackpropagationNeuralNetwork was defined and partially implemented. calculateGradients methods are to be implemented in future versions.

--- mnn/ExplicitlyLinkedNetwork/NeuralNetwork.cpp
+++ mnn/ExplicitlyLinkedNetwork/NeuralNetwork.cpp
@@ -1,5 +1,28 @@
 #include "NeuralNetwork.hpp"
+#include "mnn/exceptions.hpp"
+DefineNewMNNException(UnimplementedFeature);
+
 void mnn::ExplicitlyLinkedNeuralNetwork::process() {
 	for (auto &neuron : m_outputs)
 		neuron->value(true);
+}
+
+void mnn::ExplicitlyLinkedBackpropagationNeuralNetwork::process() {
+	for (auto &neuron : m_outputs)
+		neuron->value(true);
+}
+
+void mnn::ExplicitlyLinkedBackpropagationNeuralNetwork::calculateGradients(NeuronContainer<Value> const& _outputs) {
+	//TO DO
+	throw Exceptions::UnimplementedFeature();
+}
+void mnn::ExplicitlyLinkedBackpropagationNeuralNetwork::calculateGradients(NeuronContainer<std::shared_ptr<NeuronInterface>> const& _outputs) {
+	//TO DO
+	throw Exceptions::UnimplementedFeature();
+}
+void mnn::ExplicitlyLinkedBackpropagationNeuralNetwork::updateWeights() {
+	for (auto &n : m_hidden)
+		n->recalculateWeights();
+	for (auto &n : m_outputs)
+		std::dynamic_pointer_cast<BackpropagationNeuronInterface>(n)->recalculateWeights();
 }
--- mnn/ExplicitlyLinkedNetwork/NeuralNetwork.hpp
+++ mnn/ExplicitlyLinkedNetwork/NeuralNetwork.hpp
@@ -21,4 +21,28 @@ namespace mnn {
 			m_hidden.push_back(neuron);
 		}
 	};
+
+	class BackpropagationNeuronInterface;
+
+	class ExplicitlyLinkedBackpropagationNeuralNetwork : public BackpropagationNeuralNetworkInterface {
+	protected:
+		NeuronContainer<std::shared_ptr<BackpropagationNeuronInterface>> m_hidden;
+	public:
+		virtual void process() override;
+		using NeuralNetworkInterface::process;
+
+		virtual void calculateGradients(NeuronContainer<Value> const& _outputs) override;
+		virtual void calculateGradients(NeuronContainer<std::shared_ptr<NeuronInterface>> const& _outputs) override;
+		virtual void updateWeights() override;
+
+		inline void add_input(std::shared_ptr<BackpropagationNeuronInterface> neuron) {
+			m_inputs.push_back(neuron);
+		}
+		inline void add_output(std::shared_ptr<BackpropagationNeuronInterface> neuron) {
+			m_outputs.push_back(neuron);
+		}
+		inline void add_hidden(std::shared_ptr<BackpropagationNeuronInterface> neuron) {
+			m_hidden.push_back(neuron);
+		}
+	};
 }
