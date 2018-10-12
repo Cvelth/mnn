4b2781e9adc8d5d92d765700e7449cf487d38ff3
cvelth <cvelth.mail@gmail.com>
2018-10-12 18:51:41 +0300

    mnn::MatrixLayeredNeuralNetwork was implemented.

--- mnn/MatrixLayeredNeuralNetwork/NeuralNetwork.cpp
+++ mnn/MatrixLayeredNeuralNetwork/NeuralNetwork.cpp
@@ -1,4 +1,21 @@
 #include "NeuralNetwork.hpp"
+#include "layer.hpp"
 void mnn::MatrixLayeredNeuralNetwork::process() {
-	//TO DO
+	if (m_layers.back()->size() != m_outputs.size())
+		throw Exceptions::MatrixStructureIsBroken("Size of the last layer must be equal to number of outputs.");
+	
+	auto current = m_inputs;
+	for (auto &layer : m_layers)
+		current = layer->process(current);
+	
+	auto it1 = m_outputs.begin();
+	auto it2 = current.cbegin();
+	while (it1 != m_outputs.end() || it2 != current.cend()) {
+		*it1 = *it2;
+		it1++; it2++;
+	}
+}
+
+void mnn::MatrixLayeredNeuralNetwork::add_layer(size_t const& size, bool bias, Value const& minimum_weight_value, Value const& maximum_weight_value) {
+	m_layers.push_back(std::make_shared<Layer>(size, m_layers.empty() ? m_inputs.size() : m_layers.size(), bias, minimum_weight_value, maximum_weight_value));
 }
--- mnn/MatrixLayeredNeuralNetwork/NeuralNetwork.hpp
+++ mnn/MatrixLayeredNeuralNetwork/NeuralNetwork.hpp
@@ -1,21 +1,21 @@
 #pragma once
 #include <memory>
 #include "mnn/interfaces/NeuralNetworkInterface.hpp"
+#include "mnn/exceptions.hpp"
+DefineNewMNNException(MatrixStructureIsBroken);
 namespace mnn {
-	class NeuronInterface;
-
+	class Layer;
 	class MatrixLayeredNeuralNetwork : public NeuralNetworkInterface {
 	protected:
-		//LayerContainer
+		LayerContainer<std::shared_ptr<Layer>> m_layers;
 	public:
+		using NeuralNetworkInterface::NeuralNetworkInterface;
 		virtual void process() override;
 		using NeuralNetworkInterface::process;
 
-		inline void add_input(std::shared_ptr<NeuronInterface> neuron) {
-			m_inputs.push_back(neuron);
-		}
-		inline void add_output(std::shared_ptr<NeuronInterface> neuron) {
-			m_outputs.push_back(neuron);
-		}
+		inline LayerContainer<std::shared_ptr<Layer>> layers() { return m_layers; }
+		inline LayerContainer<std::shared_ptr<Layer>> const& layers() const { return m_layers; }
+
+		void add_layer(size_t const& size, bool bias = true, Value const& minimum_weight_value = 0.0, Value const& maximum_weight_value = 1.0);
 	};
 }
