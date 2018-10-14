1068aede9801d5f61b1e53351dca9ca3af053809
cvelth <cvelth.mail@gmail.com>
2018-10-14 23:40:15 +0300

    Storage was implemented.

--- Storage/Storage.cpp
+++ Storage/Storage.cpp
@@ -1,1 +1,36 @@
 #include "Storage.hpp"
+#include "mnn/version.hpp"
+#include <fstream>
+#include "mnn/interfaces/NeuralNetworkInterface.hpp"
+void mnn::save_to_file(std::string filename, NeuralNetworkInterface const& network) {
+	std::ofstream f(filename, std::ofstream::out);
+	if (!f) throw Exceptions::FileAccessError();
+	f << "MNN " << get_version() << '\n';
+	f << network;
+}
+#include "mnn/ExplicitlyLinkedNetwork/NeuralNetwork.hpp"
+#include "mnn/MatrixLayeredNeuralNetwork/NeuralNetwork.hpp" 
+std::unique_ptr<mnn::NeuralNetworkInterface> mnn::load_from_file(std::string filename) {
+	std::ifstream f(filename, std::ifstream::in);
+	if (!f) throw Exceptions::FileAccessError();
+	std::string version;
+	f >> version;
+	if (version != get_version()) {
+		throw Exceptions::UnsupportedFileError();
+	}
+
+	short type;
+	f >> type;
+	switch (typecodes(type)) {
+		case typecodes::explicitly_linked_network:
+			 return std::make_unique<ExplicitlyLinkedNeuralNetwork>(f);
+		case typecodes::explicitly_linked_network_backpropagation:
+			return std::make_unique<ExplicitlyLinkedBackpropagationNeuralNetwork>(f);
+		case typecodes::matrix_layered_network:
+			return std::make_unique<MatrixLayeredNeuralNetwork>(f);
+		case typecodes::matrix_layered_network_backpropagation:
+			return std::make_unique<MatrixLayeredBackpropagationNeuralNetwork>(f);
+		default:
+			throw Exceptions::UnsupportedFileError();
+	}
+}
--- Storage/Storage.hpp
+++ Storage/Storage.hpp
@@ -1,1 +1,31 @@
 #pragma once
+#include <string>
+#include "mnn/exceptions.hpp"
+DefineNewMNNException(FileAccessError);
+DefineNewMNNException(UnsupportedFileError);
+namespace mnn {
+	class NeuralNetworkInterface;
+	void save_to_file(std::string filename, NeuralNetworkInterface const& network);
+	std::unique_ptr<NeuralNetworkInterface> load_from_file(std::string filename);
+}
+
+namespace mnn {
+	enum class typecodes {
+		error = 0x00,
+
+		//NeuralNetwork codes.
+		explicitly_linked_network = 0x02,
+		explicitly_linked_network_backpropagation = 0x03,
+
+		matrix_layered_network = 0x04,
+		matrix_layered_network_backpropagation = 0x05,
+
+		//Neuron codes.
+		neuron = 0x12,
+		neuron_backpropagation = 0x13,
+
+		//Layer codes.
+		layer = 0x22,
+		layer_backpropagation = 0x23,
+	};
+}
